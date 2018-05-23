2d7d188b6d89284a3fa48a2269086ed1405954ec
lorenzwalthert <lorenz.walthert@icloud.com>
2018-05-23 15:16:06 +0200

    # namespace

--- R/data-sets.R
+++ R/data-sets.R
@@ -1,5 +1,3 @@
-#' A transfered dataset
-#' @docType dataset
 #' @include convert.R utils.R
 mtcars_converted  <- mtcars %>%
   transform_cols(c("vs", "am"), transformer = "readr::parse_factor", levels = 1:0) %>%
