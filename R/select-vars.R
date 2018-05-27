145f6f6c6eb242b1ed51a0c7b27cdb18451c3a16
lorenzwalthert <lorenz.walthert@icloud.com>
2018-05-27 18:29:20 +0200

    get rid of lengthy variable checks and use tidyselect warnings again.

--- R/select-vars.R
+++ R/select-vars.R
@@ -1,33 +1,14 @@
 #' Select variables
 #'
-#' Wrapper around [tidyselect::vars_select()].
+#' Wrapper around [tidyselect::vars_select()] with some pre-and post-processing.
+#' Most notable, for any number of selected varaiable
 #' @importFrom rlang abort quo_text
 #' @importFrom purrr map2 map_chr
 #' @importFrom utils combn
 vars_from_quo <- function(quos, data, k_dimensional) {
-  peeled <- map(quos, ~ rlang::get_expr(.x))
-  class <- map_chr(peeled, ~ class(.x)[1])
-  if (!all(class %in% c("name", "call", "character", "integer", "numeric"))) {
-    abort(paste(
-      "variables selected in ... must be either bare (e.g. vs),",
-      "quoted (e.g. 'vs'), selected with a tidyselect verb",
-      "(e.g. contains('vs') or indicate a column index."
-    ))
-  }
-  peeled <- map2(peeled, class, function(var, class) {
-    if (class %in% "name") {
-      quo_text(var)
-    } else {
-      var
-  }})
-  peeled <- tidyselect::vars_select(names(data), !!! peeled) %>% unname()
-  if (k_dimensional == 1) {
-    unlist(peeled)
-  } else {
-    peeled %>%
-      unlist() %>%
-      unname() %>%
-      combn(2, simplify = FALSE) %>%
-      map(unlist)
-  }
+  tidyselect::vars_select(names(data), !!! quos) %>%
+    unlist() %>%
+    unname() %>%
+    combn(k_dimensional, simplify = FALSE) %>%
+    map(unlist)
 }
