5faa5114c1c1c1c18391ba0ec63966fd39a08ec9
lorenzwalthert <lorenz.walthert@icloud.com>
2018-05-27 21:57:00 +0200

    fix error

--- R/assert.R
+++ R/assert.R
@@ -4,7 +4,7 @@ check_aes <- function(aes, data_names) {
   if (any(not_in_names)) {
     abort(paste(
       "The following aesthetics are not present in data: ",
-      paste(data_names[not_in_names], collapse = ", ")
+      paste(aes[not_in_names], collapse = ", ")
     ))
   }
 }
